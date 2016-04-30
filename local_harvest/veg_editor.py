c9d084a1fa4e28c6dffc80e27d79c43e903c6396
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2016-04-30 13:12:10 -0700

    added veg data editor:

--- local_harvest/veg_editor.py
+++ local_harvest/veg_editor.py
@@ -2,15 +2,20 @@ def veg_param_editor(veggies='data/veg_data.txt'):
     with open(veggies, 'r+') as infile:
         vegs = eval(infile.read())
         for veg in vegs:
-            for item in vegs[veg]:
-                print(veg, ":", item, "=", vegs[veg][item] )
-                new_value = input(">")
-                if new_value:
-                    vegs[veg][item] = new_value
+            edit = input('Edit: {}? Y/N '.format(veg))
+            if edit.lower() == 'y':
+                for attr in vegs[veg]:
+                    attr_edit = input('{} = {}, Edit? Y/N '.format(attr, vegs[veg][attr]))
+                    if attr_edit.lower() == 'y':
+                        new_attr = input('New value for {} {}: '.format(veg, attr))
+                        vegs[veg][attr] = new_attr
+                        print(veg, attr, '= ', vegs[veg][attr])
 
-
-
-
-
+                    else:
+                        pass
+            else:
+                pass
+        with open('data/veg_data_edit.txt', 'w') as f:
+            f.write(str(vegs))
 
 veg_param_editor()
